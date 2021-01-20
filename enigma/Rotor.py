8fad5da21611efd9e2664fb5d9dd84ed8e7e6583
thatsddr <domenicodiruocco01@gmail.com>
2021-01-20 20:26:21 +0100

    removed useless stuff

--- enigma/Rotor.py
+++ enigma/Rotor.py
@@ -16,18 +16,14 @@ class Rotor:
         for char in self.rotor:
             new_wiring.append(self.alphabet[(self.alphabet.index(char) + self.ringstellung) % 26])
             self.dotpos = (self.dotpos + self.ringstellung) % 26
-        j = 0
         new_wiring = "".join(new_wiring)
         while new_wiring[self.dotpos] != self.alphabet[self.ringstellung]:
-            j += 1;
             new_wiring = new_wiring[-1:] + new_wiring[:-1]
         self.rotor = new_wiring
-        #print(f"rotor after applying ringstellung: {self.rotor}, with dot position: {self.dotpos}")
     
     def rotate(self, step=1):
         if self.rotating == True: #checks if the rotor is not the zusatzwalze
             self.rotor = self.rotor[-step:] + self.rotor[:-step]
-        #print(f"current rotor posiiton after rotation of {step}: {self.rotor}")
     
     def return_letter(self, letter):
         index = self.alphabet.index(letter)
