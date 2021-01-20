ae33e390cad97b5c2aa3c7a4259934df4c63b1c0
thatsddr <domenicodiruocco01@gmail.com>
2021-01-20 16:24:00 +0100

    added reflector

--- enigma/main.py
+++ enigma/main.py
@@ -26,6 +26,15 @@ zusatzwalze = {
     "gamma": "fsokanuerhmbtiycwlqpzxvgjd",
 }
 
+class Reflector:
+    def __init__(self, ukw_type, alphabet):
+        self.type = ukw_type
+        self.alphabet = alphabet
+    
+    def reflect_letter(self, letter):
+        index = alphabet.index(letter)
+        return self.type[index]
+
 class Rotor:
     def __init__(self, rotor, alphabet, starting_pos=0, ringstellung=0, rotating=True):
         self.alphabet = alphabet
@@ -33,10 +42,10 @@ class Rotor:
         self.dotpos = self.rotor.index("a")
         self.ringstellung = ringstellung
         self.rotating = True
-        if starting_pos >= 0 and starting_pos <= 25:
-            self.pos = starting_pos
         if self.ringstellung != 0 and self.ringstellung > 0 and self.ringstellung <= 25:
             self.apply_ringstellung()
+        if starting_pos >= 0 and starting_pos <= 25:
+            self.rotate(starting_pos)
     
     def apply_ringstellung(self):
         new_wiring = []
@@ -49,17 +58,16 @@ class Rotor:
             j += 1;
             new_wiring = new_wiring[-1:] + new_wiring[:-1]
         self.rotor = new_wiring
-        print(self.rotor, self.dotpos)
+        print(f"rotor after applying ringstellung: {self.rotor}, with dot position: {self.dotpos}")
     
-    def rotate(self):
+    def rotate(self, step=1):
         if self.rotating == True: #checks if the rotor is not the zusatzwalde
-            if self.pos < 25:
-                self.pos += 1
-            else:
-                self.pos = 0
+            self.rotor = self.rotor[-step:] + self.rotor[:-step]
+        print(f"current rotor posiiton after rotation of {step}: {self.rotor}")
     
-    def return_letter(self):
-        return self.rotor[self.pos]
+    def return_letter(self, letter):
+        index = self.alphabet.index(letter)
+        return self.rotor[index]
 
 class Plugboard:
     def __init__(self, couples, text):
@@ -88,12 +96,17 @@ class Plugboard:
             else:
                 new_text.append(c)
         self.text = "".join(new_text)
-        print(self.text)
+        
 
+r = Reflector(reflectors["UKW-B-thin"], alphabet)
+print(r.reflect_letter("a"))
 
-p = Plugboard(["fa", "hb", "er", "gv", "ip", "nz", "qt", "uj", "xm", "wl"], "era una gioranta soleggiata")
+p = Plugboard(["fa", "hb", "er", "gv", "ip", "nz", "qt", "uj", "xm", "wl"], "the quick broen fox jumps over the lazy dog")
 p.apply()
-p.apply()
+print(p.text)
 
 
-#t = Rotor(rotors["I"], alphabet, ringstellung=0, starting_pos=23)
+t = Rotor(rotors["I"], alphabet, ringstellung=1, starting_pos=12)
+t.rotate()
+print(t.return_letter("a"))
+print(t.return_letter("c"))
