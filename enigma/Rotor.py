72f34a28934052057d300eb59063abc76a0713a7
thatsddr <domenicodiruocco01@gmail.com>
2021-01-24 11:51:26 +0100

    major changes

--- enigma/Rotor.py
+++ enigma/Rotor.py
@@ -1,34 +1,39 @@
 class Rotor:
     def __init__(self, confObj):
         self.alphabet = "abcdefghijklmnopqrstuvwxyz"
-        self.rotor = confObj.get("rotor")
-        self.dotpos = self.rotor.index("a")
+        self.rotor = confObj.get("rotor").get("sequence")
         self.ringstellung = confObj.get("ringstellung", 0)
         self.starting_pos = confObj.get("starting_pos", 0)
-        self.rotating = confObj.get("rotating", True)
+        self.steps = 0
         if self.ringstellung != 0 and self.ringstellung > 0 and self.ringstellung <= 25:
             self.apply_ringstellung()
-        if self.starting_pos >= 0 and self.starting_pos <= 25:
-            self.rotate(self.starting_pos)
+        if self.starting_pos > 0 and self.starting_pos <= 25:
+            self.rotate(self.starting_pos)  
+        self.notches = [self.rotor[self.alphabet.index(n)] for n in confObj.get("rotor").get("notches")]
     
     def apply_ringstellung(self):
-        new_wiring = []
-        for char in self.rotor:
-            new_wiring.append(self.alphabet[(self.alphabet.index(char) + self.ringstellung) % 26])
-            self.dotpos = (self.dotpos + self.ringstellung) % 26
-        new_wiring = "".join(new_wiring)
-        while new_wiring[self.dotpos] != self.alphabet[self.ringstellung]:
-            new_wiring = new_wiring[-1:] + new_wiring[:-1]
-        self.rotor = new_wiring
+        self.rotor = self.rotor[-self.ringstellung:] + self.rotor[:-self.ringstellung]
+        self.steps = 26 - self.ringstellung
     
+    def updateSteps(self, num=1):
+        if self.steps + num > 25:
+            self.steps = (self.steps + num) % 26
+        else:
+            self.steps += num
+
     def rotate(self, step=1):
-        if self.rotating == True: #checks if the rotor is not the zusatzwalze
-            self.rotor = self.rotor[-step:] + self.rotor[:-step]
+        self.rotor = self.rotor[step:] + self.rotor[:step]
+        self.updateSteps(step)
+        return self.rotor
     
-    def return_letter(self, letter):
+    #encrypts the letter that goes from the input towards the reflectos
+    def left_mov(self, letter, prev=None):
         index = self.alphabet.index(letter)
-        return self.rotor[index]
-    
-    def return_reverse(self, letter):
-        index = self.rotor.index(letter)
-        return self.alphabet[index]
+        if prev == None:
+            return self.rotor[index]
+        else:
+            return self.rotor[(26 - (prev - index)) % 26]
+        
+    #encrypts the letter that goes from the reflector towards the output
+    def right_mov(self, letter):
+        return self.alphabet[self.rotor.index(self.alphabet[(self.alphabet.index(letter) + self.steps) % 26])]
