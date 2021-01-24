a713a998814b5a79c7f89f43e07af7ae7570a361
thatsddr <domenicodiruocco01@gmail.com>
2021-01-24 18:08:33 +0100

    formatted

--- enigma/Enigma.py
+++ enigma/Enigma.py
@@ -13,7 +13,6 @@ class Enigma:
         self.text = text.strip().replace(" ", "").lower()
         self.plugboardConfig = configObj.get("plugboard", [])
         self.plugboard = Plugboard(self.plugboardConfig, self.text)
-        self.iteration = 1
 
     def rotateRotors(self):
         self.rotor1.rotate()
@@ -21,11 +20,10 @@ class Enigma:
             self.rotor2.rotate()
         if self.rotor2.rotor[0] in self.rotor2.notches:
             self.rotor3.rotate()
-        self.iteration += 1
 
     def encryptLetter(self, letter):
-        v1=self.rotor1.left_mov(letter)
-        v2= self.rotor2.left_mov(v1, self.rotor1.steps)
+        v1 = self.rotor1.left_mov(letter)
+        v2 = self.rotor2.left_mov(v1, self.rotor1.steps)
         v3 = self.rotor3.left_mov(v2, self.rotor2.steps)
         v4 = self.zusatzwalze.left_mov(v3, self.rotor3.steps)
         v5 = self.reflector.reflect(v4, self.zusatzwalze.steps)
@@ -35,26 +33,44 @@ class Enigma:
         v9 = self.rotor1.right_mov(v8)
         return v9
 
-    #like the method above, but prints a lot of useful information for debugging
+    # like the 'encryptLetter' method above, but prints a lot of useful information for debugging
     def log_encypt(self, letter):
-        v1=self.rotor1.left_mov(letter)
-        print(f"{self.rotor1.alphabet.upper()}\n{self.rotor1.rotor.upper()}\nAfter rotor 1: {v1.upper()}\n")
-        v2= self.rotor2.left_mov(v1, self.rotor1.steps)
-        print(f"{self.rotor2.alphabet.upper()}\n{self.rotor2.rotor.upper()}\nAfter rotor 2: {v2.upper()}\nprev: {self.rotor1.steps}\n")
+        v1 = self.rotor1.left_mov(letter)
+        print(
+            f"{self.rotor1.alphabet.upper()}\n{self.rotor1.rotor.upper()}\nAfter rotor 1: {v1.upper()}\n"
+        )
+        v2 = self.rotor2.left_mov(v1, self.rotor1.steps)
+        print(
+            f"{self.rotor2.alphabet.upper()}\n{self.rotor2.rotor.upper()}\nAfter rotor 2: {v2.upper()}\nprev: {self.rotor1.steps}\n"
+        )
         v3 = self.rotor3.left_mov(v2, self.rotor2.steps)
-        print(f"{self.rotor3.alphabet.upper()}\n{self.rotor3.rotor.upper()}\nAfter rotor 3: {v3.upper()}\nprev: {self.rotor2.steps}\n")
+        print(
+            f"{self.rotor3.alphabet.upper()}\n{self.rotor3.rotor.upper()}\nAfter rotor 3: {v3.upper()}\nprev: {self.rotor2.steps}\n"
+        )
         v4 = self.zusatzwalze.left_mov(v3, self.rotor3.steps)
-        print(f"{self.zusatzwalze.alphabet.upper()}\n{self.zusatzwalze.rotor.upper()}\nAfter zusatzwalze: {v4.upper()}\nprev: {self.rotor3.steps}\n")
+        print(
+            f"{self.zusatzwalze.alphabet.upper()}\n{self.zusatzwalze.rotor.upper()}\nAfter zusatzwalze: {v4.upper()}\nprev: {self.rotor3.steps}\n"
+        )
         v5 = self.reflector.reflect(v4, self.zusatzwalze.steps)
-        print(f"{self.rotor1.alphabet}\n{self.reflector.type}\nAfter reflector: {v5.upper()}\nprev: {self.zusatzwalze.steps}\n")
+        print(
+            f"{self.rotor1.alphabet}\n{self.reflector.type}\nAfter reflector: {v5.upper()}\nprev: {self.zusatzwalze.steps}\n"
+        )
         v6 = self.zusatzwalze.right_mov(v5)
-        print(f"{self.zusatzwalze.rotor.upper()}\n{self.zusatzwalze.alphabet.upper()}\nAfter inversed zusatzwalze: {v6.upper()}\n")
+        print(
+            f"{self.zusatzwalze.rotor.upper()}\n{self.zusatzwalze.alphabet.upper()}\nAfter inversed zusatzwalze: {v6.upper()}\n"
+        )
         v7 = self.rotor3.right_mov(v6)
-        print(f"{self.rotor3.rotor.upper()}\n{self.rotor3.alphabet.upper()}\nAfter inversed rotor 3: {v7.upper()}\n")
+        print(
+            f"{self.rotor3.rotor.upper()}\n{self.rotor3.alphabet.upper()}\nAfter inversed rotor 3: {v7.upper()}\n"
+        )
         v8 = self.rotor2.right_mov(v7)
-        print(f"{self.rotor2.rotor.upper()}\n{self.rotor2.alphabet.upper()}\nAfter inversed rotor 2: {v8.upper()}\n")
+        print(
+            f"{self.rotor2.rotor.upper()}\n{self.rotor2.alphabet.upper()}\nAfter inversed rotor 2: {v8.upper()}\n"
+        )
         v9 = self.rotor1.right_mov(v8)
-        print(f"{self.rotor1.rotor.upper()}\n{self.rotor1.alphabet.upper()}\nAfter inversed rotor 1: {v9.upper()}\n")
+        print(
+            f"{self.rotor1.rotor.upper()}\n{self.rotor1.alphabet.upper()}\nAfter inversed rotor 1: {v9.upper()}\n"
+        )
         print("--------------------------")
         return v9
 
