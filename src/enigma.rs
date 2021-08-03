4165829aa3b8806180d268287909ead275dc7014
thatsddr <domenicodiruocco01@gmail.com>
2021-08-03 20:27:25 +0200

    added run

--- src/enigma.rs
+++ src/enigma.rs
@@ -47,7 +47,7 @@ impl Enigma {
         }
     }
 
-    pub fn encrypt_letter(&mut self, letter: char) -> char {
+    fn encrypt_letter(&mut self, letter: char) -> char {
         let v1 = self.rotor1.left_mov(letter, 0);
         let v2 = self.rotor2.left_mov(v1, self.rotor1.steps);
         let v3 = self.rotor3.left_mov(v2, self.rotor2.steps);
@@ -59,14 +59,33 @@ impl Enigma {
         let v9 = self.rotor1.right_mov(v8);
         v9
     }
+
+    pub fn run(&mut self, text: String) -> String {
+        let to_process = self.plugboard.apply(text);
+        let chars_text: Vec<char> = to_process.chars().collect();
+        let mut temp = vec![];
+        let mut temp_length: i32 = 0;
+        for i in 0..chars_text.len() {
+            if self.settings.alphabet.contains(chars_text[i]) {
+                temp_length += 1;
+                self.rotate_rotors();
+                temp.push(self.encrypt_letter(chars_text[i]));
+                if temp_length % 4 == 0 {
+                    temp_length = 0;
+                    temp.push(' ');
+                }
+            }
+        }
+        let tempstr: String = temp.into_iter().collect();
+        self.plugboard.apply(tempstr)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn should_create_a_new_enigma() {
+    fn generate_enigma() -> Enigma {
         let c = ConfStruct {
             reflector: "UKW-B-thin".to_owned(),
             zus: RotorInput {
@@ -103,48 +122,17 @@ mod tests {
             ]
             .to_vec(),
         };
-        Enigma::new(c);
+        return Enigma::new(c);
+    }
+
+    #[test]
+    fn should_create_a_new_enigma() {
+        generate_enigma();
     }
 
     #[test]
     fn should_rotate_the_rotors() {
-        let c = ConfStruct {
-            reflector: "UKW-B-thin".to_owned(),
-            zus: RotorInput {
-                rot: "gamma".to_owned(),
-                pos: 'p',
-                ring: 11,
-            },
-            rot3: RotorInput {
-                rot: "VI".to_owned(),
-                pos: 'q',
-                ring: 21,
-            },
-            rot2: RotorInput {
-                rot: "II".to_owned(),
-                pos: 'l',
-                ring: 6,
-            },
-            rot1: RotorInput {
-                rot: "IV".to_owned(),
-                pos: 'e',
-                ring: 13,
-            },
-            plugboard: [
-                "bq".to_owned(),
-                "cr".to_owned(),
-                "di".to_owned(),
-                "ej".to_owned(),
-                "kw".to_owned(),
-                "mt".to_owned(),
-                "os".to_owned(),
-                "px".to_owned(),
-                "uz".to_owned(),
-                "gh".to_owned(),
-            ]
-            .to_vec(),
-        };
-        let mut e = Enigma::new(c);
+        let mut e = generate_enigma();
         for n in 1..100 {
             e.rotate_rotors()
         }
@@ -152,44 +140,15 @@ mod tests {
     }
     #[test]
     fn should_encrypt_the_letter() {
-        let c = ConfStruct {
-            reflector: "UKW-B-thin".to_owned(),
-            zus: RotorInput {
-                rot: "gamma".to_owned(),
-                pos: 'p',
-                ring: 11,
-            },
-            rot3: RotorInput {
-                rot: "VI".to_owned(),
-                pos: 'q',
-                ring: 21,
-            },
-            rot2: RotorInput {
-                rot: "II".to_owned(),
-                pos: 'l',
-                ring: 6,
-            },
-            rot1: RotorInput {
-                rot: "IV".to_owned(),
-                pos: 'e',
-                ring: 13,
-            },
-            plugboard: [
-                "bq".to_owned(),
-                "cr".to_owned(),
-                "di".to_owned(),
-                "ej".to_owned(),
-                "kw".to_owned(),
-                "mt".to_owned(),
-                "os".to_owned(),
-                "px".to_owned(),
-                "uz".to_owned(),
-                "gh".to_owned(),
-            ]
-            .to_vec(),
-        };
-        let mut e = Enigma::new(c);
+        let mut e = generate_enigma();
         let r = e.encrypt_letter('h');
         assert_eq!(r, 'z')
+    }
+
+    #[test]
+    fn should_run() {
+        let mut e = generate_enigma();
+        let r = e.run(String::from("have fun using this"));
+        panic!("{:?}", r)
     }
 }
