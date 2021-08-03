28269c6e132cbf4f2892051a8fb055c2aef4e246
thatsddr <domenicodiruocco01@gmail.com>
2021-08-03 20:03:22 +0200

    added encrypt letter function

--- src/enigma.rs
+++ src/enigma.rs
@@ -46,6 +46,19 @@ impl Enigma {
             self.rotor3.rotate(1);
         }
     }
+
+    pub fn encrypt_letter(&mut self, letter: char) -> char {
+        let v1 = self.rotor1.left_mov(letter, 0);
+        let v2 = self.rotor2.left_mov(v1, self.rotor1.steps);
+        let v3 = self.rotor3.left_mov(v2, self.rotor2.steps);
+        let v4 = self.zusatzwalze.left_mov(v3, self.rotor3.steps);
+        let v5 = self.reflector.reflect(v4, self.zusatzwalze.steps);
+        let v6 = self.zusatzwalze.right_mov(v5);
+        let v7 = self.rotor3.right_mov(v6);
+        let v8 = self.rotor2.right_mov(v7);
+        let v9 = self.rotor1.right_mov(v8);
+        v9
+    }
 }
 
 #[cfg(test)]
@@ -136,5 +149,47 @@ mod tests {
             e.rotate_rotors()
         }
         assert_eq!(e.rotor2.sequence, "siruxblhwtmcqgznpyfvoeajdk".to_string())
+    }
+    #[test]
+    fn should_encrypt_the_letter() {
+        let c = ConfStruct {
+            reflector: "UKW-B-thin".to_owned(),
+            zus: RotorInput {
+                rot: "gamma".to_owned(),
+                pos: 'p',
+                ring: 11,
+            },
+            rot3: RotorInput {
+                rot: "VI".to_owned(),
+                pos: 'q',
+                ring: 21,
+            },
+            rot2: RotorInput {
+                rot: "II".to_owned(),
+                pos: 'l',
+                ring: 6,
+            },
+            rot1: RotorInput {
+                rot: "IV".to_owned(),
+                pos: 'e',
+                ring: 13,
+            },
+            plugboard: [
+                "bq".to_owned(),
+                "cr".to_owned(),
+                "di".to_owned(),
+                "ej".to_owned(),
+                "kw".to_owned(),
+                "mt".to_owned(),
+                "os".to_owned(),
+                "px".to_owned(),
+                "uz".to_owned(),
+                "gh".to_owned(),
+            ]
+            .to_vec(),
+        };
+        let mut e = Enigma::new(c);
+        let r = e.encrypt_letter('h');
+        assert_eq!(r, 'z')
     }
 }
