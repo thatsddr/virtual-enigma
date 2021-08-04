af76a343d8fadc880d7a1bb8f6918bd5dfddadf6
thatsddr <domenicodiruocco01@gmail.com>
2021-08-04 11:07:06 +0200

    reintroduced logging

--- enigma/Enigma.py
+++ enigma/Enigma.py
@@ -36,9 +36,31 @@ class Enigma:
         v8 = self.rotor2.right_mov(v7)
         v9 = self.rotor1.right_mov(v8)
         return v9
+    
+    def log_encypt(self, letter):
+         v1=self.rotor1.left_mov(letter)
+         print(f"{self.rotor1.alphabet.upper()}\n{self.rotor1.rotor.upper()}\nAfter rotor 1: {v1.upper()}\n")
+         v2= self.rotor2.left_mov(v1, self.rotor1.steps)
+         print(f"{self.rotor2.alphabet.upper()}\n{self.rotor2.rotor.upper()}\nAfter rotor 2: {v2.upper()}\nprev: {self.rotor1.steps}\n")
+         v3 = self.rotor3.left_mov(v2, self.rotor2.steps)
+         print(f"{self.rotor3.alphabet.upper()}\n{self.rotor3.rotor.upper()}\nAfter rotor 3: {v3.upper()}\nprev: {self.rotor2.steps}\n")
+         v4 = self.zusatzwalze.left_mov(v3, self.rotor3.steps)
+         print(f"{self.zusatzwalze.alphabet.upper()}\n{self.zusatzwalze.rotor.upper()}\nAfter zusatzwalze: {v4.upper()}\nprev: {self.rotor3.steps}\n")
+         v5 = self.reflector.reflect_letter(v4)
+         print(f"{self.rotor1.alphabet}\n{self.reflector.type}\nAfter reflector: {v5.upper()}\n")
+         v6 = self.zusatzwalze.right_mov(v5)
+         print(f"{self.zusatzwalze.rotor.upper()}\n{self.zusatzwalze.alphabet.upper()}\nAfter inversed zusatzwalze: {v6.upper()}\n")
+         v7 = self.rotor3.right_mov(v6)
+         print(f"{self.rotor3.rotor.upper()}\n{self.rotor3.alphabet.upper()}\nAfter inversed rotor 3: {v7.upper()}\n")
+         v8 = self.rotor2.right_mov(v7)
+         print(f"{self.rotor2.rotor.upper()}\n{self.rotor2.alphabet.upper()}\nAfter inversed rotor 2: {v8.upper()}\n")
+         v9 = self.rotor1.right_mov(v8)
+         print(f"{self.rotor1.rotor.upper()}\n{self.rotor1.alphabet.upper()}\nAfter inversed rotor 1: {v9.upper()}\n")
+         print("--------------------------")
+         return v9
 
 
-    def run(self, text):
+    def run(self, text, logging=False):
         self.text = self.plugboard.apply(text.strip().replace(" ", "").lower())
         temp = []
         temp_length = 0
@@ -46,7 +68,10 @@ class Enigma:
             if self.text[i] in self.settings.alphabet:
                 temp_length += 1
                 self.rotateRotors()
-                temp.append(self.encryptLetter(self.text[i]))
+                if logging==False:
+                    temp.append(self.encryptLetter(self.text[i]))
+                else:
+                    temp.append(self.log_encrypt(self.text[i]))
                 if temp_length % 4 == 0:
                     temp_length = 0
                     temp.append(" ")
